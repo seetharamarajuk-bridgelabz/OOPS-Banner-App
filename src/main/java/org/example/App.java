031e9c60dfc2b01ae7bb122ed038b8da6f4e85de
seetharamarajuk-bridgelabz <seetharamaraju.k@bridgelabz.com>
2026-01-29 20:20:58 +0530

    UC1: Print OOPS to Console

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,17 @@
+package org.example;
+
+/**
+ * OOPSBannerApp UC1 - OOPS Banner Display Application
+ * <p>
+ * This Class demostrates a simple Java application that displays the object
+ * Oriented Programming System OOPS acronym to the console.
+ *
+ * @author seetharamaraju
+ * @version 1.0
+ */
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
