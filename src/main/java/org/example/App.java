74dcf1160509cca9bf80f254a403673e46f8bd3b
seetharamarajuk-bridgelabz <seetharamaraju.k@bridgelabz.com>
2026-01-29 20:40:50 +0530

    UC2: Render OOPS as Banner using Print Statements

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,17 +1,55 @@
 package org.example;
 
 /**
- * OOPSBannerApp UC1 - OOPS Banner Display Application
+ * OOPSBannerApp UC2 - Render OOPS as Banner (spaces and *)
+ * <p>
+ * This Class demostrates the creation of ASSCII art banner using Object-Oriented Programming Principles
+ * It extends the basic banner concept by displaying the word "OOPS" in a visual format using asterisks(*)
+ * and spaces to form each letter
+ * <p>
  * <p>
  * This Class demostrates a simple Java application that displays the object
  * Oriented Programming System OOPS acronym to the console.
  *
  * @author seetharamaraju
- * @version 1.0
+ * @version 2.0
+ */
+
+/**
+ *  Extend the User Story 1 to display the message "OOPS" in a banner format
+ *  Using characters formed by spaces and asterisks(*) to create a visual
+ *  effect for each letter in the message.
  */
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("OOPS");
+        System.out.println(
+                " ******** " + "  ******** " + " ******** " + "  ********"
+        );
+
+        System.out.println(
+                "*        *" + " *        *" + " *       *" + " *        "
+        );
+
+        System.out.println(
+                "*        *" + " *        *" + " *       *" + " *        "
+        );
+
+        System.out.println(
+                "*        *" + " *        *" + " ******** " + "  ********"
+        );
+
+        System.out.println(
+                "*        *" + " *        *" + " *        " + "         *"
+        );
+
+        System.out.println(
+                "*        *" + " *        *" + " *        " + "         *"
+        );
+
+        System.out.println(
+                " ******** " + "  ******** " + " *        " + "  ********"
+        );
+
     }
 }
